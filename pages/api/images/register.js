7643dccdaeab936c845f4fa6c4500791f87be42a
Tobi Olanitori <tobiolanitori@gmail.com>
2023-01-03 12:28:13 +0100

    FEAT: added an endpoint to get user pattern image

--- pages/api/images/register.js
+++ pages/api/images/register.js
@@ -17,6 +17,16 @@ export default async function handler(req, res) {
         await foundUser.save();
 
         res.status(200).json({success: true, message: 'image upload successful'})
+    } else if(method==="GET") {
+        const auth = req.headers.authorization;
+        if(!auth) return res.status(401).json({success: false, message: 'resource not available to user'});
+
+        const email = auth.split(" ")[1];
+        const foundUser = await User.findOne({email}).exec();
+        if(!foundUser) return res.status(404).json({ success: false, message: `user with ${email} not found` });
+
+        const authImage = foundUser.authImage.imageString
+        res.status(200).json({success: true, data: authImage})
     } else {
         res.status(404).json({ success: false, message: "resource not found" });
     }
