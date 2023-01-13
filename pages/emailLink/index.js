8959f2db25317dbce3deed559407c2ace3db362c
Tobi Olanitori <tobiolanitori@gmail.com>
2023-01-13 08:41:13 +0100

    FEAT: Added UI for sent link page

--- pages/emailLink/index.js
+++ pages/emailLink/index.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import { useRouter } from "next/router";
+import { HiArrowNarrowLeft } from "react-icons/hi";
 
 const EmailLink = () => {
+    const router = useRouter();
   return (
-    <main>
-        
+    <main style={{ height: "100vh" }} className="app__flex main">
+      <article className="column-flex modal">
+        <div className="modal-arrow">
+          <HiArrowNarrowLeft onClick={() =>router.back()} />
+        </div>
+        <h1 style={{ marginBottom: "8px" }} className="head-text">
+          Link Sent
+        </h1>
+        <p style={{ marginBottom: "40px" }} className="p-text-2">
+          Please kindly check your email for a password reset link and follow
+          the instruction
+        </p>
+        <div
+          style={{  marginBottom: "16px" }}
+          className="modal-btn column-flex"
+        >
+          <button className="primary-btn">Go to Email</button>
+        </div>
+        <div
+          style={{ marginTop: "22px", marginBottom: "156px" }}
+          className="modal-btn column-flex"
+        >
+          <button className="tertiary-btn">Back to Home</button>
+        </div>
+        <div className="modal-footer app__flex-2">
+          <p>Privacy and Policy</p>
+          <p>FAQ</p>
+        </div>
+      </article>
     </main>
-  )
-}
+  );
+};
 
 export default EmailLink;
--- pages/forgot-password/index.js
+++ pages/forgot-password/index.js
@@ -4,34 +4,35 @@ import { useRouter } from "next/router";
 import { HiArrowNarrowLeft } from "react-icons/hi";
 
 const ForgotPassword = () => {
-    const [email, setEmail] = useState("");
-    const [emailErrMsg, setEmailErrMsg] = useState("");
-    const router = useRouter();
-    const emailRef = useRef();
+  const [email, setEmail] = useState("");
+  const [emailErrMsg, setEmailErrMsg] = useState("");
+  const router = useRouter();
+  const emailRef = useRef();
 
-    useEffect(() => {
-        setEmailErrMsg("");
-      }, [email]);
+  useEffect(() => {
+    setEmailErrMsg("");
+  }, [email]);
 
-    useEffect(() => {
-        emailRef.current.focus();
-      }, []);
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
 
-      const handleForgotPassword = () => {
-        router.push("/emailLink");
-      }
+  const handleForgotPassword = () => {
+    router.push("/emailLink");
+  };
 
   return (
     <main style={{ height: "100vh" }} className="app__flex main">
       <article className="column-flex modal">
         <div className="modal-arrow">
-          <HiArrowNarrowLeft />
+          <HiArrowNarrowLeft onClick={() => router.back()} />
         </div>
         <h1 style={{ marginBottom: "8px" }} className="head-text">
-        Forgot Your Password?
+          Forgot Your Password?
         </h1>
         <p style={{ marginBottom: "20px" }} className="p-text-2">
-        Enter your email address in the form below and we will send you further instruction on how to reset your password
+          Enter your email address in the form below and we will send you
+          further instruction on how to reset your password
         </p>
         <div className={emailErrMsg ? "modal-input error" : "modal-input"}>
           <input
